ee2ca3f8a3a8d58f1bb5ccf9f32e8704ec93110b
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-31 16:50:20 +0530

    Added code to get text file of following

---
+++ list-following.py
@@ -0,0 +1,48 @@
+import requests
+
+def get_following(username, token, per_page=100):
+    following = []
+    page = 1
+
+    while True:
+        url = f'https://api.github.com/users/{username}/following'
+        params = {
+            'per_page': per_page,
+            'page': page
+        }
+        headers = {
+            'Authorization': f'token {token}'
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        
+        if response.status_code != 200:
+            print(f'Error fetching following: {response.status_code}')
+            break
+
+        data = response.json()
+        if not data:
+            break
+
+        following.extend(data)
+        page += 1
+
+    return following
+
+def save_following_to_file(following, filename='following.txt'):
+    with open(filename, 'w') as file:
+        for user in following:
+            file.write(f'{user["login"]}\n')
+
+def main():
+    # Replace 'your_github_username' with your actual GitHub username
+    username = 'your_github_username'
+    # Replace 'your_github_token' with your personal GitHub token
+    token = 'your_github_token'
+
+    following = get_following(username, token)
+    save_following_to_file(following)
+    print(f'Successfully saved {len(following)} following users to following.txt')
+
+if __name__ == '__main__':
+    main()
